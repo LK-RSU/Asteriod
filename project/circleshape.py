6c135d96fb5e9af705382d958aaa28ba627f23e3
LK-RSU <krit.ja69@rsu.ac.th>
2026-06-18 12:54:36 +0700

    add collide function to circleshape

--- project/circleshape.py
+++ project/circleshape.py
@@ -15,6 +15,11 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def collides_with(self, other) -> bool:
+        # check distance between two circles
+        distance = self.position.distance_to(other.position)
+        return distance <= self.radius + other.radius
+
     def draw(self, screen: pygame.Surface) -> None:
         # must override
         pass
--- project/main.py
+++ project/main.py
@@ -1,38 +1,55 @@
+import sys
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0.0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(x, y)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    # AsteroidField only needs to update to run its spawning timer
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     running = True
 
     while running:
         log_state()
-        dt = clock.tick(60) / 1000  # limits FPS to 60 and calculates delta time
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    # fill the screen with a color to wipe away anything from last frame
-        screen.fill("black")
     # UPDATE YOUR GAME HERE & RENDER YOUR GAME HERE
         updatable.update(dt)
-        for thing in drawable:
-            thing.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                # log, print, and exit here
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit(0)
+    # fill the screen with a color to wipe away anything from last frame
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
     # flip() the display to put your work on screen
         pygame.display.flip()
+    # limits FPS to 60 and calculates delta time
+        dt = clock.tick(60) / 1000  
     pygame.quit()
 
 
